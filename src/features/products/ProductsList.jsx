41e15860916715b7ba7e0948c278f6babe090b2f
lukmanxpert <lukmanmiah2004@gmail.com>
2025-03-29 05:06:39 +0600

    rednder all the products

--- src/features/products/ProductsList.jsx
+++ src/features/products/ProductsList.jsx
@@ -6,11 +6,25 @@ const ProductsList = () => {
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(fetchProducts())
-    }, [])
+    }, [dispatch])
     const { products, isLoading, isError } = useSelector((state) => state.productsR)
     return (
-        <div>
-            <h1>Products List</h1>
+        <div className=''>
+            <h1 className='text-center text-2xl font-bold my-2 underline'>Products List {`(${products.length})`}</h1>
+            {isLoading && <h1 className='text-lg text-center font-bold text-green-500'>Loading...</h1>}
+            {isError && <h1>{isError}</h1>}
+            <section className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4'>
+                {
+                    !isLoading && !isError && products.length > 0 && products.map((product) => {
+                        return <article key={product.id} className='bg-slate-300 p-2 rounded-lg'>
+                            <h1 className='font-bold text-xl'>ID: {product.id}</h1>
+                            <h1 className='font-bold text-xl'>Title: {product.title}</h1>
+                            <p>Description: {product.description}</p>
+                            <p className='font-bold'>Price: {product.price}</p>
+                        </article>
+                    })
+                }
+            </section>
         </div>
     );
 };
